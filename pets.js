eaa0268fd325e4e27157f187835d7f613a8a738b
Tuğcan Özaktaş <tugcanozaktas@hotmail.com>
2023-04-19 21:00:28 +0300

    Completed the virtual pet exercisegit status

--- pets.js
+++ pets.js
@@ -1,58 +1,158 @@
-module.exports = 
-    class Pet {
-        constructor(name, age, hunger, fitness){
-            this.name = name;
-            this.age = age;
-            this.happiness = 0;
-            this.hunger = hunger;
-            this.fitness = fitness;
+const MAXIMUM_FITNESS = 10;
+const FITNESS_INCR = 4
+
+const MINIMUM_HUNGER = 0;
+const HUNGER_DECR = 3;
+
+function Pet(name){
+    this.name = name;
+    this.age = 0;
+    this.hunger = 0;
+    this.fitness = 10;
+    this.children = [];
+}
+
+Pet.prototype = {
+    
+    get isAlive() {
+        return this.age < 30 && this.hunger < 10 && this.fitness > 0;
+    }
+    
+}
+
+Pet.prototype.growUp = function(){
+    if(!(this.isAlive)){
+        throw new Error("Your pet is no longer alive :(")
+    } else{
+        this.age += 1;
+        this.hunger += 5;
+        this.fitness -= 3;
+    }
+}
+
+Pet.prototype.walk = function(){
+    if(!(this.isAlive)){
+        throw new Error("Your pet is no longer alive :(")
+    } else{
+        if((this.fitness + FITNESS_INCR) <= MAXIMUM_FITNESS){
+            this.fitness += FITNESS_INCR;
+        }else{
+            this.fitness = MAXIMUM_FITNESS;
         }
+    }
 
-        incrementAge() {
-            this.age += 1;
-            this.hunger += 1;
-            this.fitness -= 1;
+}
+
+Pet.prototype.feed = function(){
+    if(!(this.isAlive)){
+        throw new Error("Your pet is no longer alive :(")
+    } else{
+        if((this.hunger - HUNGER_DECR) >= MINIMUM_HUNGER){
+            this.hunger -= HUNGER_DECR;
+        }else{
+            this.hunger = MINIMUM_HUNGER;
         }
+    }
+    
+}
 
-        showLove(){
-            this.happiness += 1;
+Pet.prototype.checkUp = function(){
+    if(!(this.isAlive)){
+        throw new Error("Your pet is no longer alive :(")
+    } else{
+        if(this.fitness <= 3 && this.hunger >= 5){
+            return "I am hungry AND I need a walk";
+        } else if(this.fitness <= 3){
+            return "I need a walk";
+        } else if(this.hunger >= 5){
+            return "I am hungry";
+        } else{
+            return "I feel great!";
         }
+    }
+    
+}
 
-        isAlive(){
-            if(this.age >= 30){
-                return false
-            }else if(this.hunger > 15){
-                return false
-            }else if(this.fitness < 0){
-                return false
-            }
+Pet.prototype.haveBaby = function(babyName){
+    const baby = new Pet(babyName);
+    this.children.push(baby);
+}
 
-            return true
-        }
 
-        feed() {
-            if(this.hunger >= 0){
-                this.hunger -= 1;
-            }
-        }
 
-        walk(){
-            if(this.fitness <= 15){
-                this.fitness += 1;
-            }
-        }
 
-        talk(){
-            let difHunger = 15 - this.hunger;
+
+
+    
+module.exports = Pet;
+
     
 
-            if(difHunger < this.fitness){
-                return "I need food!!!"
-            }else if(this.fitness < difHunger){
-                return "I need to walk!!!"
-            }
 
-        
+
+
+
+
+
+
+
+
+
+/*
+class Pet{
+    constructor(name, age, hunger, fitness){
+        this.name = name;
+        this.age = age;
+        this.happiness = 0;
+        this.hunger = hunger;
+        this.fitness = fitness;
+    }
+
+    incrementAge() {
+        this.age += 1;
+        this.hunger += 1;
+        this.fitness -= 1;
+    }
+
+    showLove(){
+        this.happiness += 1;
+    }
+
+    isAlive(){
+        if(this.age >= 30){
+            return false
+        }else if(this.hunger > 15){
+            return false
+        }else if(this.fitness < 0){
+            return false
         }
+
+        return true
+    }
+
+    feed() {
+        if(this.hunger >= 0){
+            this.hunger -= 1;
+        }
+    }
+
+    walk(){
+        if(this.fitness <= 15){
+            this.fitness += 1;
+        }
+    }
+
+    talk(){
+        let difHunger = 15 - this.hunger;
+
+
+        if(difHunger < this.fitness){
+            return "I need food!!!"
+        }else if(this.fitness < difHunger){
+            return "I need to walk!!!"
+        }
+
     
     }
+}
+*/
--- tests/pets.test.js
+++ tests/pets.test.js
@@ -1,7 +1,226 @@
-const Pet = require("../pets")
+const Pet = require("../pets");
 
-const dog = new Pet("pamuk", 1, 8, 6);
 
+
+
+describe("constructor", ()=>{
+    it("returns an object", ()=>{
+        expect(new Pet("Fido")).toBeInstanceOf(Object);
+    })
+
+    it("sets the name property", ()=>{
+        const pet = new Pet("Fido");
+        expect(pet.name).toEqual("Fido");
+    })
+
+    it("has an initial age of 0", ()=>{
+        const pet = new Pet("Fido");
+        expect(pet.age).toEqual(0);
+    })
+
+    it("has an initial hunger of 0", ()=>{
+        const pet = new Pet("Fido");
+        expect(pet.hunger).toEqual(0);
+    })
+    it("has an initial fitness of 10", ()=>{
+        const pet = new Pet("Fido");
+        expect(pet.fitness).toEqual(10);
+    })
+})
+
+describe("growUp", ()=>{
+    it("should return pet is not alive if conditions are not satisfied", ()=>{
+        const pet = new Pet("dally");
+
+        pet.age = 31;
+
+
+        expect(() =>{pet.checkUp()}).toThrow("Your pet is no longer alive :(");
+    })
+    it("increments the age by 1", ()=>{
+        const pet = new Pet("Limo");
+
+        pet.growUp();
+
+        expect(pet.age).toEqual(1);
+    })
+
+    it("increments the hunger by 5", ()=>{
+        const pet = new Pet("Limo");
+
+        pet.growUp();
+
+        expect(pet.hunger).toEqual(5);
+    })
+
+    it("decrements the fitness by 3", ()=>{
+        const pet = new Pet("Limo");
+
+        pet.growUp();
+
+        expect(pet.fitness).toEqual(7);
+    })
+    
+})
+
+describe("walk", ()=>{
+    it("should return pet is not alive if conditions are not satisfied", ()=>{
+        const pet = new Pet("dally");
+
+        pet.fitness = -1;
+
+
+        expect(() =>{pet.checkUp()}).toThrow("Your pet is no longer alive :(");
+    })
+
+    it("should increment fitness by 4 and not exceed 10",()=>{
+        const pet = new Pet("Fido");
+
+        pet.fitness = 9;
+
+        pet.walk();
+
+        expect(pet.fitness).toEqual(10);
+    })
+
+    it("should increment fitness by 4", ()=>{
+        const pet = new Pet("Fido");
+
+        pet.fitness = 2;
+        
+        pet.walk();
+
+        expect(pet.fitness).toEqual(6);
+    })
+})
+
+describe("feed", ()=>{
+    it("should return pet is not alive if conditions are not satisfied", ()=>{
+        const pet = new Pet("dally");
+
+        pet.hunger = 11;
+
+
+        expect(() =>{pet.checkUp()}).toThrow("Your pet is no longer alive :(");
+    })
+    it("should decrement hunger by 3 and not go below 0",()=>{
+        const pet = new Pet("Fido");
+
+        pet.hunger = 2;
+
+        pet.feed();
+
+        expect(pet.hunger).toEqual(0);
+    })
+
+    it("should decrement hunger by 3", ()=>{
+        const pet = new Pet("Fido");
+
+        pet.hunger = 5;
+        
+        pet.feed();
+
+        expect(pet.hunger).toEqual(2);
+    })
+})
+
+describe("checkUp", ()=>{
+    it("should return walk if pets fitness is 3 or less", ()=>{
+        const pet = new Pet("dally");
+
+        pet.hunger = 3;
+        pet.fitness = 3;
+
+        expect(pet.checkUp()).toEqual("I need a walk");
+    })
+    it("should return hungry if pets hunger is 5 or more", ()=>{
+        const pet = new Pet("dally");
+        
+        pet.fitness = 6;
+        pet.hunger = 6;
+
+        expect(pet.checkUp()).toEqual("I am hungry");
+    })
+    it("should return hungry and walk if fitness <= 3 and hunger >= 5", ()=>{
+        const pet = new Pet("dally");
+
+        pet.hunger = 6;
+        pet.fitness = 2;
+
+        expect(pet.checkUp()).toEqual("I am hungry AND I need a walk");
+    })
+
+    it("should return great if fitness > 3 and hunger < 5", ()=>{
+        const pet = new Pet("dally");
+
+        pet.hunger = 4;
+        pet.fitness = 5;
+
+        expect(pet.checkUp()).toEqual("I feel great!");
+    })
+
+    it("should return pet is not alive if conditions are not satisfied", ()=>{
+        const pet = new Pet("dally");
+
+        pet.hunger = 11;
+
+
+        expect(() =>{pet.checkUp()}).toThrow("Your pet is no longer alive :(");
+    })
+
+
+})
+
+describe("isAlive", ()=>{
+    it("returns false if fitness is 0 or less", ()=>{
+        const pet = new Pet("dally");
+        
+        pet.fitness = -3;
+
+        expect(pet.isAlive).toBe(false)
+    })
+    it("returns false if hunger is 10 or more", ()=>{
+        const pet = new Pet("dally");
+        
+        pet.hunger = 12;
+
+        expect(pet.isAlive).toBe(false)
+    })
+    it("returns false if age is 30 or more", ()=>{
+        const pet = new Pet("dally");
+        
+        pet.age = 30;
+
+        expect(pet.isAlive).toBe(false)
+    })
+    it("returns true otherwise", ()=>{
+        const pet = new Pet("dally");
+        
+        pet.fitness = 5;
+        pet.hunger = 5;
+        pet.age = 5;
+
+        expect(pet.isAlive).toBe(true)
+    })
+})
+
+describe("haveBaby", ()=>{
+    it("should add a baby named dilly in children array", ()=>{
+        const pet = new Pet("dally");
+        pet.haveBaby("dilly");
+        
+        expect(pet.children).toBeInstanceOf(Array);
+        
+        expect(pet.children[0].name).toBe("dilly")
+
+    })
+})
+
+
+
+
+
+/*
 describe("Pet Information", () =>{
 
     
@@ -96,3 +315,4 @@ describe("Checks status of dog", ()=>{
         expect(dog.isAlive()).toEqual(false);
     });
 });
+*/
